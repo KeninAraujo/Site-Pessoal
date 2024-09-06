e2ed4b49ad8e80d386b166b0a686c095b489ea6b
Kenin Rodrigues <kenin.rodrigues@example.com>
2024-09-06 18:13:18 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,45 @@
+class MobileNavbar {
+  constructor(mobileMenu, navlist, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navlist = document.querySelector(navlist);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      let delay = index / 7 + 0.3;
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.3s ease forwards ${delay}s`);
+    });
+  }
+
+  handleClick() {
+    this.navlist.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    console.log(this);
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu ",
+  ".nav-list",
+  ".nav-list li"
+);
+
+mobileNavbar.init();
